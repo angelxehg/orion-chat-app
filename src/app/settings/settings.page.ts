deddfbe5a5a628126a0a55f7aaaaac46a4bb2b8d
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-23 13:51:39 -0500

    Allow to change api path

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GatewayService } from '../services/gateway.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit {
 
-  constructor() { }
+  api_path: string;
+
+  constructor(private gateway: GatewayService) { }
+
+  ngOnInit() {
+    this.api_path = this.gateway.api_path;
+  }
+
+  defaultPath() {
+    this.api_path = "http://192.168.0.62:8000/api/v1/";
+    this.savePath();
+  }
+
+  savePath() {
+    this.gateway.api_path = this.api_path;
+  }
 
 }
