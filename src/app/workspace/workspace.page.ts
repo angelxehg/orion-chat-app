e194a1fb9829e99184c729dc55692d19cded8752
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-27 12:25:11 -0500

    Drop Friend example

--- src/app/workspace/workspace.page.ts
+++ src/app/workspace/workspace.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FriendService } from '../services/friend.service';
 
 @Component({
   selector: 'app-workspace',
@@ -8,37 +7,7 @@ import { FriendService } from '../services/friend.service';
 })
 export class WorkspacePage implements OnInit {
 
+  constructor() { }
 
-  constructor(
-    public friendService: FriendService) { }
-
-  ngOnInit() {
-    // this.friendService.loadAll();
-  }
-
-  ionViewWillEnter() {
-    // Used ionViewWillEnter as ngOnInit is not 
-    // called due to view persistence in Ionic
-    this.friendService.loadAll();
-  }
-
-  doRefresh(event) {
-    console.log('Begin async operation');
-    //Get saved list of friends
-    this.friendService.loadAllThen(event);
-
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.target.complete();
-    }, 2000);
-  }
-
-  delete(item) {
-    //Delete item in Friend data
-    this.friendService.deleteItem(item.id).subscribe(Response => {
-      //Update list after delete is successful
-      this.friendService.loadAll();
-    });
-  }
-
+  ngOnInit() { }
 }
