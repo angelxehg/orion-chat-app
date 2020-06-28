7428a9e6043502c12319686da0f0c0648026f360
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-06-28 11:02:18 -0500

    Catch Login request errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { BehaviorSubject, Observable, from, of } from 'rxjs';
-import { take, map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { JwtHelperService } from "@auth0/angular-jwt";
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -52,26 +52,14 @@ export class AuthService {
         if (res) {
           this.jwt_access = res.access;
           this.jwt_refresh = res.refresh;
+          console.log("Saving token at Storage...");
           let storageObs = from(this.storage.set("TOKEN_REFRESH", res.refresh));
+          console.log("Saved token at Storage!");
           return storageObs;
         }
         return of(null);
       })
     );
-    // return this.http.get('https://randomuser.me/api/').pipe(
-    //   take(1),
-    //   map(res => {
-    //     // Extract the JWT, here we just fake it
-    //     return `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1Njc2NjU3MDYsImV4cCI6MTU5OTIwMTcwNiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiMTIzNDUiLCJmaXJzdF9uYW1lIjoiU2ltb24iLCJsYXN0X25hbWUiOiJHcmltbSIsImVtYWlsIjoic2FpbW9uQGRldmRhY3RpYy5jb20ifQ.4LZTaUxsX2oXpwN6nrSScFXeBNZVEyuPxcOkbbDVZ5U`;
-    //   }),
-    //   switchMap(token => {
-    //     let decoded = helper.decodeToken(token);
-    //     this.userData.next(decoded);
-
-    //     let storageObs = from(this.storage.set("TOKEN_REFRESH", token));
-    //     return storageObs;
-    //   })
-    // );
   }
 
   getUser() {
--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -18,23 +18,33 @@ export class LoginPage implements OnInit {
   constructor(
     private auth: AuthService,
     private router: Router,
-    private alertCtrl: AlertController) { }
+    public toastController: ToastController) { }
 
   ngOnInit() { }
 
   login() {
-    this.auth.login(this.credentials).subscribe(async res => {
-      if (res) {
-        this.router.navigateByUrl('/app');
-      } else {
-        const alert = await this.alertCtrl.create({
-          header: 'Login Failed',
-          message: 'Wrong credentials.',
-          buttons: ['OK']
-        });
-        await alert.present();
-      }
+    this.auth.login(this.credentials).subscribe({
+      next: data => this.loginSuccess(data),
+      error: error => this.loginError(error)
     });
+  }
+
+  loginSuccess(data) {
+    this.toast("Successful login!");
+    this.router.navigateByUrl('/app');
+  }
+
+  loginError(error) {
+    this.toast("Login error: " + error.message);
+    console.log(error);
+  }
+
+  async toast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
