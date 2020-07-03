51a8c556e2fe1f166b8a4ce18f0a6e6b8b04e2c0
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-02 20:51:39 -0500

    Use list groups & windows.path

--- src/app/panel/panel.page.ts
+++ src/app/panel/panel.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-panel',
@@ -11,46 +9,65 @@ export class PanelPage implements OnInit {
 
   public selectedIndex = 0;
 
-  constructor(private menu: MenuController, private activatedRoute: ActivatedRoute) { }
-
-  public pages = [
+  public pageGroups = [
     {
-      title: 'Workspaces',
-      url: '/app/workspaces',
-      icon: 'briefcase'
+      title: "Main",
+      pages: [
+        {
+          title: 'Workspaces',
+          url: '/app/workspaces',
+          icon: 'briefcase',
+        },
+        {
+          title: 'Channels',
+          url: '/app/channels',
+          icon: 'chatbubbles',
+        },
+      ]
     },
     {
-      title: 'Channels',
-      url: '/app/channels',
-      icon: 'chatbubbles'
+      title: "Features",
+      pages: [
+        {
+          title: 'Search',
+          url: '/app/search',
+          icon: 'search',
+        },
+        {
+          title: 'Files',
+          url: '/app/files',
+          icon: 'folder',
+        },
+      ]
     },
     {
-      title: 'Search',
-      url: '/app/search',
-      icon: 'search'
-    },
-    {
-      title: 'Organization',
-      url: '/app/organization',
-      icon: 'people'
-    },
-    {
-      title: 'Profile',
-      url: '/app/profile',
-      icon: 'person-circle'
-    },
-    {
-      title: 'Settings',
-      url: '/app/settings',
-      icon: 'cog'
-    },
-  ];
-
-  ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.pages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      title: "Configuration",
+      pages: [
+        {
+          title: 'Organization',
+          url: '/app/organization',
+          icon: 'people',
+        },
+        {
+          title: 'Profile',
+          url: '/app/profile',
+          icon: 'person-circle',
+        },
+        {
+          title: 'Settings',
+          url: '/app/settings',
+          icon: 'cog',
+        },
+      ]
     }
+  ]
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  isUrlActive(url) {
+    return window.location.pathname.includes(url);
   }
 
 }
