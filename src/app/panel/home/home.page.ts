ba97ef1a5322370516688f20a15a8745365e891a
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-06 10:57:51 -0500

    Pinned items in home page

--- src/app/panel/home/home.page.ts
+++ src/app/panel/home/home.page.ts
@@ -9,6 +9,65 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class HomePage {
 
+  private tileGroups = [
+    {
+      tiles: [
+        {
+          title: "All workspaces",
+          url: "/app/workspaces",
+          icon: "briefcase",
+          color: "primary",
+        },
+        {
+          title: "Workspace 1",
+          url: "/app/workspaces/1",
+          icon: "briefcase",
+          color: "primary",
+        },
+        {
+          title: "Workspace 2",
+          url: "/app/workspaces/2",
+          icon: "briefcase",
+          color: "primary",
+        },
+        {
+          title: "Workspace 3",
+          url: "/app/workspaces/3",
+          icon: "briefcase",
+          color: "primary",
+        },
+      ]
+    },
+    {
+      tiles: [
+        {
+          title: "All channels",
+          url: "/app/channels",
+          icon: "chatbubbles",
+          color: "success",
+        },
+        {
+          title: "Channel 1",
+          url: "/app/channels/1",
+          icon: "chatbubbles",
+          color: "success",
+        },
+        {
+          title: "Channel 2",
+          url: "/app/channels/2",
+          icon: "chatbubbles",
+          color: "success",
+        },
+        {
+          title: "Channel 3",
+          url: "/app/channels/3",
+          icon: "chatbubbles",
+          color: "success",
+        },
+      ]
+    },
+  ];
+
   constructor(
     private auth: AuthService,
     public panel: PanelService
