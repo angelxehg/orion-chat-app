90c448a6dfc2832eca19d332e1bd6633f2b7acd9
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-07 11:51:57 -0500

    Create register form

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
 
-  constructor() { }
+  credentials = {
+    username: '',
+    email: '',
+    password: '',
+    re_password: ''
+  };
 
-  ngOnInit() {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    public toastController: ToastController) { }
+
+  register() {
+    this.toast("Attempting to register...");
+    // this.auth.register(this.credentials).subscribe({
+    //   next: data => this.registerSuccess(data),
+    //   error: error => this.registerError(error)
+    // });
+  }
+
+  registerSuccess(data) {
+    this.toast("Successful register: 200");
+    this.router.navigateByUrl('/app');
+  }
+
+  registerError(error) {
+    this.toast("register error: " + error.status + ": " + error.statusText);
+    console.error(error);
+  }
+
+  async toast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
