d02d5ae40ed6fea40b38bea1ff2135fb912244a6
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-07-18 09:41:02 -0500

    Create, Update, Delete channels

--- src/app/pages/channels/channel-details/channel-details.page.ts
+++ src/app/pages/channels/channel-details/channel-details.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PanelService } from '../../../services/panel.service';
 import { PageData } from '../../../models/page-data';
 import { ChannelService } from 'src/app/services/channel.service';
+import { Channel } from 'src/app/models/channel';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-channel-details',
@@ -11,60 +13,191 @@ import { ChannelService } from 'src/app/services/channel.service';
 })
 export class ChannelDetailsPage {
 
-  public page: PageData;
+  public channel: Channel;
+
+  private mode: string;
+
+  public error;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     public panel: PanelService,
-    public chn: ChannelService
+    public chn: ChannelService,
+    private router: Router,
+    public toastController: ToastController,
+    public alertController: AlertController
   ) {
-    this.emptyMode();
+    this.clear();
+    this.mode = 'Create';
+  }
+
+  get title() {
+    if (this.mode == 'Read') {
+      return this.channel.title;
+    }
+    return this.mode + " channel";
+  }
+
+  get description() {
+    if (this.mode == 'Read') {
+      return this.channel.title + " channel data";
+    }
+    if (this.mode == 'Update') {
+      return "Update channel data";
+    }
+    return "Create a new channel";
+  }
+
+  get createMode() {
+    return this.mode == 'Create';
+  }
+
+  get readMode() {
+    return this.mode == 'Read';
+  }
+
+  get updateMode() {
+    return this.mode == 'Update';
   }
 
   ionViewWillEnter() {
+    this.clear();
     this.panel.show('channels', false);
     var param = this.activatedRoute.snapshot.paramMap.get('channel');
-    if (!param) {
-      if (window.location.pathname.includes("new")) {
-        // Create mode
-        this.createMode();
-        this.chn.fetch().subscribe();
-      } else {
-        // Empty mode
-        this.emptyMode();
-      }
+    if (param) {
+      this.mode = 'Read';
+      var thisID = parseInt(param);
+      this.chn.find(thisID).subscribe({
+        next: (found) => {
+          this.channel = Object.create(found);
+        }
+      });
     } else {
-      // Edit mode
-      this.editMode();
+      this.mode = 'Create';
       this.chn.fetch().subscribe();
     }
   }
 
-  emptyMode() {
-    this.page = {
-      title: "Channel Details",
-      description: "Create or select a channel...",
-      action: "Empty",
-      empty: true
-    };
+
+  public edit() {
+    this.mode = 'Update';
   }
 
-  createMode() {
-    this.page = {
-      title: "Create a channel",
-      description: "Create a new channel",
-      action: "Create",
-      empty: false
-    };
+  public cancel() {
+    this.clearError();
+    if (this.createMode) {
+      this.router.navigateByUrl(`/app/channels`);
+    }
+    if (this.updateMode) {
+      this.mode = 'Read';
+    }
   }
 
-  editMode() {
-    this.page = {
-      title: "Edit channel",
-      description: "Update channel data",
-      action: "Update",
-      empty: false
-    };
+  public save() {
+    this.clearError();
+    if (this.createMode) {
+      this.create();
+    }
+    if (this.updateMode) {
+      this.update();
+    }
   }
 
+  public async delete() {
+    if (!this.updateMode) {
+      return
+    }
+    const alert = await this.alertController.create({
+      header: 'Remove this channel?',
+      message: 'All channel data will be removed',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Remove',
+          handler: () => {
+            this.remove();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  private async create() {
+    var toast = await this.toast("Creating Channel data...");
+    this.chn.create(this.channel).subscribe({
+      next: async (created) => {
+        toast.dismiss().then(() => this.toast("Channel created!", 'success', true));
+        this.router.navigateByUrl(`/app/channels/${created.id}`);
+      },
+      error: async (err) => {
+        toast.dismiss().then(() => this.toast("Error creating channel!", 'danger', true));
+        if ('error' in err) {
+          this.error = err.error;
+        }
+        console.error(err);
+      }
+    });
+  }
+
+  private async update() {
+    var toast = await this.toast("Updating Channel data...");
+    this.chn.update(this.channel).subscribe({
+      next: async (updated) => {
+        toast.dismiss().then(() => this.toast("Channel data updated!", 'success', true));
+        this.mode = 'Read';
+      },
+      error: async (err) => {
+        toast.dismiss().then(() => this.toast("Error updating data!", 'danger', true));
+        if ('error' in err) {
+          this.error = err.error;
+        }
+        console.error(err);
+      }
+    });
+  }
+
+  private async remove() {
+    var toast = await this.toast("Removing channel...", 'warning');
+    this.chn.remove(this.channel).subscribe({
+      next: async () => {
+        toast.dismiss().then(() => this.toast("Channel removed!", 'success', true));
+        this.router.navigateByUrl(`/app/channels`);
+      },
+      error: async (err) => {
+        toast.dismiss().then(() => this.toast("Error removing channel!", 'danger', true));
+        console.error(err);
+      }
+    });
+  }
+
+  private async toast(message: string, color: string = 'dark', dismiss: boolean = false) {
+    var buttons = !dismiss ? [] : [{
+      text: 'Close',
+      role: 'cancel'
+    }]
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 5000,
+      color: color,
+      buttons: buttons
+    });
+    toast.present();
+    return toast;
+  }
+
+  clear() {
+    this.clearError();
+    this.channel = new Channel();
+  }
+
+  clearError() {
+    this.error = {
+      title: '',
+      description: ''
+    }
+  }
 }
--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -3,7 +3,7 @@ import { Channel } from '../models/channel';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { OrganizationService } from './organization.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -19,6 +19,18 @@ export class ChannelService {
     private http: HttpClient,
     private org: OrganizationService
   ) { }
+
+  public find(channelID: number) {
+    return this.fetch().pipe(
+      map(data => {
+        var channel = data.find(e => e.id == channelID);
+        if (!channel) {
+          return null;
+        }
+        return channel;
+      })
+    );
+  }
 
   public fetch() {
     if (!this.lastFetch) {
@@ -44,6 +56,45 @@ export class ChannelService {
             this.collection.next(data);
             this.lastFetch = new Date().getTime();
             return this.all();
+          })
+        ).toPromise();
+      })
+    );
+  }
+
+  public create(channel: Channel) {
+    return this.org.selected.pipe(
+      switchMap(async organization => {
+        return await this.http.post(`${environment.api_url}/organizations/${organization.id}/channels/`, channel).pipe(
+          switchMap(async (data: Channel) => {
+            await this.forceFetch().toPromise();
+            return data;
+          })
+        ).toPromise();
+      })
+    );
+  }
+
+  public update(channel: Channel) {
+    return this.org.selected.pipe(
+      switchMap(async organization => {
+        return await this.http.patch(`${environment.api_url}/organizations/${organization.id}/channels/${channel.id}/`, channel).pipe(
+          switchMap(async (data: Channel) => {
+            await this.forceFetch().toPromise();
+            return data;
+          })
+        ).toPromise();
+      })
+    );
+  }
+
+  public remove(channel: Channel) {
+    return this.org.selected.pipe(
+      switchMap(async organization => {
+        return await this.http.delete(`${environment.api_url}/organizations/${organization.id}/channels/${channel.id}/`).pipe(
+          switchMap(async () => {
+            await this.forceFetch().toPromise();
+            return true;
           })
         ).toPromise();
       })
