08f4624811219c1e1a63f293dff26f891914c1e5
Angel Hurtado <contact@angelxehg.com>
2020-10-23 17:48:07 -0500

    Jump to v2.0.0-alpha.5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: '2.0.0-alpha.4'
+  version: '2.0.0-alpha.5'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  version: '2.0.0-alpha.4'
+  version: '2.0.0-alpha.5'
 };
 
 /*
