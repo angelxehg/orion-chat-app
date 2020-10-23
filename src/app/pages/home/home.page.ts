3f18d9e4857b9611abcdd5286faabcc09b408b1c
Angel Hurtado <contact@angelxehg.com>
2020-10-23 16:16:13 -0500

    Leave Home only with color palette

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { PanelService } from '../../services/panel.service';
-import { OrganizationService } from 'src/app/services/organization.service';
-import { Organization } from 'src/app/models/organization';
 
 @Component({
   selector: 'app-home',
@@ -12,35 +10,10 @@ export class HomePage {
 
   colors = ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger'];
 
-  public organization: Organization;
-
-  public tiles = [
-    {
-      title: 'All workspaces',
-      url: '/app/workspaces',
-      icon: 'briefcase',
-      color: 'primary',
-    },
-    {
-      title: 'All channels',
-      url: '/app/channels',
-      icon: 'chatbubbles',
-      color: 'success',
-    },
-  ];
-
-  constructor(
-    public panel: PanelService,
-    public org: OrganizationService
-  ) { }
+  constructor(public panel: PanelService) { }
 
   ionViewWillEnter() {
     this.panel.show();
-    this.org.selected.subscribe({
-      next: (found) => {
-        this.organization = Object.create(found);
-      }
-    });
   }
 
 }
