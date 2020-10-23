67e50f156e314b69db561fe29ef362b88b5ea53c
Angel Hurtado <contact@angelxehg.com>
2020-10-23 13:19:18 -0500

    Drop old panel and its parts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -11,27 +10,11 @@ const routes: Routes = [
   {
     path: 'app',
     loadChildren: () => import('./layout/layout.module').then(m => m.LayoutPageModule),
-    // canActivate: [AuthGuard],
   },
   {
     path: 'login',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)
   },
-  // {
-  //   path: 'app',
-  //   loadChildren: () => import('./panel/panel.module').then(m => m.PanelPageModule),
-  //   canActivate: [AuthGuard]
-  // },
-  // {
-  //   path: 'register',
-  //   loadChildren: () => import('./pages/auth/register/register.module').then(m => m.RegisterPageModule),
-  //   canLoad: [AuthGuard]
-  // },
-  // {
-  //   path: 'login',
-  //   loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginPageModule),
-  //   canLoad: [AuthGuard]
-  // },
 ];
 @NgModule({
   imports: [
