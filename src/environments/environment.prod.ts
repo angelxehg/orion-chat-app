25bb733b8a61e6af5f11229dfdf670b2bf3cb253
Angel Hurtado <contact@angelxehg.com>
2020-11-11 12:11:10 -0600

    Jump to v2.0.0-rc.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '2.0.0-beta.3',
+  version: '2.0.0-rc.1',
   firebase: {
     apiKey: 'AIzaSyAJCX0vuYdhkEgbT53JlLGgVKueIz8iNDs',
     authDomain: 'tomatoe-chat.firebaseapp.com',
