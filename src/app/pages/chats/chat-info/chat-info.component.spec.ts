45e3a53bdc38d72067fcfc55aae9f361d96835c9
Angel Hurtado <contact@angelxehg.com>
2020-11-11 11:50:08 -0600

    fix tests

--- src/app/pages/chats/chat-info/chat-info.component.spec.ts
+++ src/app/pages/chats/chat-info/chat-info.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
+import { ChatsService, ChatServiceMock } from 'src/app/services/chats.service';
 
 import { ChatInfoComponent } from './chat-info.component';
 
@@ -9,8 +11,14 @@ describe('ChatInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatInfoComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ChatInfoComponent],
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: ChatsService, useValue: ChatServiceMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ChatInfoComponent);
